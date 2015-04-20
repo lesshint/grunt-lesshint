330039828dd6644f32f9b37c570f134dce512430
Oskar Risberg <kokarn@gmail.com>
2015-04-20 17:11:45 +0200

    Update code style to match what I like

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function( grunt ){
 
     // Project configuration.
     grunt.initConfig({
@@ -31,23 +31,23 @@ module.exports = function(grunt) {
 
         // Unit tests.
         nodeunit: {
-            tests: ['test/*_test.js']
+            tests: [ 'test/*_test.js' ]
         }
 
     });
 
     // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+    grunt.loadTasks( 'tasks' );
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
     grunt.registerTask( 'test', [ 'lesshint', 'nodeunit' ] );
 
     // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'test']);
+    grunt.registerTask( 'default', [ 'jshint', 'test' ] );
 
 };
